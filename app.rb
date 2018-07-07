b035f81ab72ba69b150bf6e336bb7089ad1656f7
Adam Kalt <adam.kalt.1@gmail.com>
2018-07-07 02:53:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,11 +12,7 @@ end
     end
 
     post '/checkout' do
-      binding.pry
-      @session[:item] = []
-      params[:checkout][:item].each do |item|
-       @session[:item] << item[:name]
-       end
+      @session[:item] = params[:item]
      end
 
 end
